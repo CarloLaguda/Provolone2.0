4cc3a68ff386799f4d7eee300a2c28dfaf08e478
CarloLaguda <carlo.laguda07@gmail.com>
2025-05-18 09:16:44 +0000

    punto 4

--- prenotazioni-Laguda/src/app/app.component.ts
+++ prenotazioni-Laguda/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Prenotation } from './Models/Prenotation.model';
 import { DatePipe, CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,31 @@ import { DatePipe, CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prenotazioni-Laguda';
+
   vett_prenotazioni: Prenotation[] = []
+  obs_vet!: Observable<Prenotation[]>
+
+  http: HttpClient
+
+  constructor(http: HttpClient){this.http = http}
+
+  loading!:boolean
+
+  makeGet()
+  {
+    this.loading = false
+    this.obs_vet = this.http.get<Prenotation[]>("https://my-json-server.typicode.com/malizia-g/verificaPrenotazioni/prenotazioni")
+    this.obs_vet.subscribe(this.getData)
+  }
+  getData = (d: Prenotation[]) =>
+  {
+    this.vett_prenotazioni = d
+    this.loading = false
+  }
+  ngOnInit() 
+  {
+    this.makeGet()
+  }
 }
