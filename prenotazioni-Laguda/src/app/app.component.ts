5b46b598070cfec850a85c628aff59eada016804
CarloLaguda <carlo.laguda07@gmail.com>
2025-05-18 09:05:27 +0000

    punto 3

--- prenotazioni-Laguda/src/app/app.component.ts
+++ prenotazioni-Laguda/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Prenotation } from './Models/Prenotation.model';
+import { DatePipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prenotazioni-Laguda';
+  vett_prenotazioni: Prenotation[] = []
 }
